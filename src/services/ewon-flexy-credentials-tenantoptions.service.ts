481e6087f7473420790b554658a297dfb56d31eb
yhegen <45969355+yhegen@users.noreply.github.com>
2022-01-05 14:54:24 +0100

    Multiple Users Credentials
    using user id encode base64 as differentiator for multiple user defined credentials.

--- src/services/ewon-flexy-credentials-tenantoptions.service.ts
+++ src/services/ewon-flexy-credentials-tenantoptions.service.ts
@@ -1,17 +1,20 @@
-import { FlexySettings } from './../interfaces/ewon-flexy-registration.interface';
 import { Injectable } from "@angular/core";
 import { AlertService } from "@c8y/ngx-components";
-import { TenantOptionsService, ITenantOption } from "@c8y/client";
+import { TenantOptionsService, ITenantOption, UserService, IUser } from "@c8y/client";
 import { FLEXY_TENANTOPTIONS_CATEGORY } from "../constants/flexy-integration.constants";
 
 @Injectable()
 export class EWONFlexyCredentialsTenantoptionsService {
   constructor(
     private tenantOptionsService: TenantOptionsService,
+    private userService: UserService,
     private alert: AlertService
   ) {}
 
   async updateCredentials(config: any) {
+
+    const user = await this.userService.current();
+    const base64 = btoa(user.data.id).replace("=","").replace("+","-").replace("/","_");
 
     const listKeys = Object.keys(config)
     for await (const iterate of listKeys) {
@@ -20,7 +23,7 @@ export class EWONFlexyCredentialsTenantoptionsService {
         continue;
       }
       const option: ITenantOption = {
-        category: FLEXY_TENANTOPTIONS_CATEGORY,
+        category: FLEXY_TENANTOPTIONS_CATEGORY + '_' + base64,
         key: iterate,
         value: config[iterate],
       };
@@ -30,14 +33,17 @@ export class EWONFlexyCredentialsTenantoptionsService {
   }
 
   async getCredentials(): Promise<ITenantOption[]> {
-      const filter = {
-      category: FLEXY_TENANTOPTIONS_CATEGORY,
+
+    const user = await this.userService.current();
+    const base64 = btoa(user.data.id).replace("=","").replace("+","-").replace("/","_");
+    const filter = {
+      category: FLEXY_TENANTOPTIONS_CATEGORY + '_' + base64,
       pageSize: 100,
       withTotalPages: true,
     };
     const { data, res, paging } = await this.tenantOptionsService.list(filter);
     const filteredData = data
-      .filter((tmp) => tmp.category === FLEXY_TENANTOPTIONS_CATEGORY)
+      .filter((tmp) => tmp.category === FLEXY_TENANTOPTIONS_CATEGORY + '_' + base64)
       .map((tmp) => tmp as ITenantOption)
       .filter((tmp) => tmp.key != "credentials.password");
       
